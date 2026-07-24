d237c90aac25030ae32d0fdbec0516ca1e5084f9
Felipe <fsouzas@live.com>
2026-07-24 04:11:21 -0300

    more than 13 players standing implemented, buttons to change between screens

--- scripts/_standings.gd
+++ scripts/_standings.gd
@@ -25,12 +25,28 @@ extends Node
 @export var standing_results_1 : VBoxContainer
 @export var standings_extra :VBoxContainer
 
+@export var tab : TabContainer
+
 @onready var standing_size : int = 3
 @onready var num_standings : int = 1
+
+@export var menu : Node
+@export var voltar_button : Button
+@export var share_button : Button
+@export var save_button : Button 
+@export var proximo_button : Button 
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	share.set_share_target(true)
+	
+	#voltar_button.self_modulate.a = 0
+	voltar_button.disabled = true
+	
+	if UniversalDict.armory_data.size() <= 13:
+		proximo_button.self_modulate.a = 0
+		proximo_button.disabled = true
 	
 	mode_name.text = UniversalDict.mode_selected
 	store_name.text = UniversalDict.store_name.to_upper() +" "+ Time.get_date_string_from_system()
@@ -80,23 +96,132 @@ func _ready() -> void:
 	else:
 		heroi_destaque3_front.texture = load(HeroesDb.young_heroes[UniversalDict.armory_data.values()[2]["Hero"]]["front"])
 	
-	SortStanding.sort_standing(self, standing_less, standings_extra, standing_results_1)
+	SortStanding.sort_standing(tab, standing_less, standings_extra, standing_results_1)
 	
 
 	
-func _on_button_pressed() -> void:
-	%Button.visible = false
-	await RenderingServer.frame_post_draw
-	save_and_share_screenshot()
 
-func save_and_share_screenshot():
+func share_screenshot():
 	share.share_viewport(get_viewport(), "shared_title", "shared_subject", "Standings de hoje!")
 	pass
 
 
 func _on_share_share_canceled() -> void:
-	%Button.visible = true
-
+	menu.visible = true
 
 func _on_share_share_completed(activity_type: String) -> void:
-	%Button.visible = true
+	menu.visible = true
+
+func _on_share_pressed() -> void:
+	menu.visible = false
+	await RenderingServer.frame_post_draw
+	share_screenshot()
+	menu.visible = true
+
+
+func _on_voltar_pressed() -> void:
+	voltar_button.disabled = true
+	share_button.disabled = true
+	save_button.disabled = true
+	proximo_button.disabled = true
+	
+	var last_node_pos = tab.get_child(tab.current_tab).offset_transform_position
+	var tween_main: Tween = create_tween().set_parallel(true)
+	tween_main.set_ease(tween_main.EASE_IN_OUT)
+	tween_main.set_trans(tween_main.TRANS_EXPO)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"offset_transform_position", Vector2(tab.get_child(tab.current_tab).offset_transform_position.x , tab.get_child(tab.current_tab).offset_transform_position.y + 100),1.0)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"modulate:a", 0, 1.0)
+	await tween_main.finished
+	if tween_main:
+		tween_main.kill()
+	tab.get_child(tab.current_tab).offset_transform_position = last_node_pos
+	
+	tab.current_tab -= 1
+	
+	print(tab.current_tab)
+	
+	last_node_pos = tab.get_child(tab.current_tab).offset_transform_position
+	tab.get_child(tab.current_tab).offset_transform_position.y += 100
+	tween_main = create_tween().set_parallel(true)
+	tween_main.set_ease(tween_main.EASE_IN_OUT)
+	tween_main.set_trans(tween_main.TRANS_EXPO)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"offset_transform_position", Vector2(tab.get_child(tab.current_tab).offset_transform_position.x , tab.get_child(tab.current_tab).offset_transform_position.y - 100),1.0)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"modulate:a", 1, 1.0)
+	await tween_main.finished
+	if tween_main:
+		tween_main.kill()
+	tab.get_child(tab.current_tab).offset_transform_position = last_node_pos
+	
+	
+	if(tab.current_tab == 0):
+		var tween: Tween = create_tween().set_parallel()
+		tween.set_ease(tween.EASE_IN_OUT)
+		tween.set_trans(tween.TRANS_EXPO)                                                                                  
+		tween.tween_property(voltar_button,"offset_transform_position",Vector2(0,100), 1.0)
+		tween.tween_property(voltar_button,"self_modulate", 0, 1.0)
+	if (tab.get_tab_count() > 0):
+		var tween: Tween = create_tween().set_parallel()                                                                              
+		tween.set_ease(tween.EASE_IN_OUT)
+		tween.set_trans(tween.TRANS_EXPO)                                                                                  
+		tween.tween_property(proximo_button,"offset_transform_position",Vector2(0,0), 1.0)
+		tween.tween_property(proximo_button,"self_modulate",1, 1.0)
+	voltar_button.disabled = false
+	share_button.disabled = false
+	save_button.disabled = false
+	proximo_button.disabled = false
+
+
+
+
+
+func _on_proximo_pressed() -> void:
+	voltar_button.disabled = true
+	share_button.disabled = true
+	save_button.disabled = true
+	proximo_button.disabled = true
+	
+	var last_node_pos = tab.get_child(tab.current_tab).offset_transform_position
+	var tween_main: Tween = create_tween().set_parallel(true)
+	tween_main.set_ease(tween_main.EASE_IN_OUT)
+	tween_main.set_trans(tween_main.TRANS_EXPO)
+	var last_node_pos_offset : Vector2 = tab.get_child(tab.current_tab).offset_transform_position
+	tween_main.tween_property(tab.get_child(tab.current_tab),"offset_transform_position", Vector2(tab.get_child(tab.current_tab).offset_transform_position.x , tab.get_child(tab.current_tab).offset_transform_position.y + 100),1.0)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"modulate:a", 0, 1.0)
+	await tween_main.finished
+	if tween_main:
+		tween_main.kill()
+	tab.get_child(tab.current_tab).offset_transform_position = last_node_pos
+	
+	tab.current_tab += 1
+	print(tab.current_tab)
+	
+	last_node_pos = tab.get_child(tab.current_tab).offset_transform_position
+	tab.get_child(tab.current_tab).offset_transform_position.y += 100
+	tween_main = create_tween().set_parallel(true)
+	tween_main.set_ease(tween_main.EASE_IN_OUT)
+	tween_main.set_trans(tween_main.TRANS_EXPO)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"offset_transform_position", Vector2(tab.get_child(tab.current_tab).offset_transform_position.x , tab.get_child(tab.current_tab).offset_transform_position.y - 100),1.0)
+	tween_main.tween_property(tab.get_child(tab.current_tab),"modulate:a", 1, 1.0)
+	await tween_main.finished
+	if tween_main:
+		tween_main.kill()
+	print(tab.get_tab_count())
+	tab.get_child(tab.current_tab).offset_transform_position = last_node_pos
+	
+	tween_main = create_tween().set_parallel()                                                                                
+	tween_main.set_ease(tween_main.EASE_IN_OUT)
+	tween_main.set_trans(tween_main.TRANS_EXPO)                                                                                  
+	tween_main.tween_property(voltar_button,"offset_transform_position",Vector2(0,0), 1.0)
+	tween_main.tween_property(voltar_button,"self_modulate",1, 1.0)
+	
+	if(tab.current_tab == tab.get_tab_count()- 1):
+		var tween: Tween = create_tween().set_parallel()                                                                              
+		tween.set_ease(tween.EASE_IN_OUT)
+		tween.set_trans(tween.TRANS_EXPO)                                                                                  
+		tween.tween_property(proximo_button,"offset_transform_position",Vector2(0,100), 1.0)
+		tween.tween_property(proximo_button,"self_modulate", 0, 1.0)
+	
+	voltar_button.disabled = false
+	share_button.disabled = false
+	save_button.disabled = false
+	proximo_button.disabled = false
